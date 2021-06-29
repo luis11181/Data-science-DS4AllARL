c0e1cd6e3abab8771378fde1c53fc5b9e4967782
luis11181 <luis11181@hotmail.com>
2021-06-29 11:43:11 -0500

    3

--- sqldata/data.py
+++ sqldata/data.py
@@ -1,4 +1,5 @@
-# psoditiva db  
+
+#* pip install -r requirements.txt -t libraries 
 #! script to connect to db from terminal
 # psql -h dbpositiva.cogq7bwdrted.us-east-2.rds.amazonaws.com -U mainuser -p 5432
 
@@ -7,11 +8,31 @@
 from sqlalchemy import create_engine #Crea la conexión que se puede usar con pandas
 import psycopg2 as pg #Crea conexión para hacer queries en general (incluso operaciones de INSERT)
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import missingno as msno  #libreria para plotear datos faltantes
 
 conn = pg.connect(user = "mainuser", password = "positivaadmin", host = "dbpositiva.cogq7bwdrted.us-east-2.rds.amazonaws.com",  port = "5432", database = "positiva")
                         
 engine = create_engine("postgresql://mainuser:positivaadmin@dbpositiva.cogq7bwdrted.us-east-2.rds.amazonaws.com/positiva")
 
-data = pd.read_sql_query("SELECT * FROM dempresas_retiradas LIMIT 5", engine) # Returns a DataFrame corresponding to the result set of the query string. Optionally provide an index_col parameter to use one of the columns as the index, otherwise default integer index will be used.
-print(data)
+Eretiradas = pd.read_sql_query("SELECT * FROM dempresas_retiradas", engine) # Returns a DataFrame corresponding to the result set of the query string. Optionally provide an index_col parameter to use one of the columns as the index, otherwise default integer index will be used.
 
+Causalretiro = pd.read_sql_query("SELECT * FROM dcausal_retiro", engine)
+
+Eretiradas = pd.read_sql_query("SELECT * FROM dcausal_retiro LIMIT 10000", engine)
+
+print('informacion basica empresas retiradas=')
+print(Eretiradas.info())
+print('datos unicos=')
+print(Eretiradas.nunique())
+print('null data:')
+print(Eretiradas.isnull().value_counts())
+
+print(r'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('informacion basica causal retiro=')
+print(Causalretiro.info())
+print('datos unicos=')
+print(Causalretiro.nunique())
+print('null data:')
+print(Causalretiro.isnull().value_counts())
